c4d88a2948375b1653afcbddc643320f9042a46d
Zach <zjanssen23@gmail.com>
2023-10-24 18:14:57 -0500

    createPassword function with error checking

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+import string
+
+def createPassword(length):
+    stringOne = string.ascii_letters + string.punctuation + string.digits
+    password = random.choices(stringOne, k=length)
+    return "".join(password)
+
+def main():
+    print("=======================")
+    print(
+      '''
+      Password Generator v1.0
+=====================================================================================================
+This little program will create a random password based on the number of characters that you give it
+All password contain upper/lower characters, numbers and punctuation
+Its suggested to have a minimum of 16 characters!!
+===================================================================================================== 
+      '''
+      )
+
+    user_answer = input('How many characters do you want the password to be?: ')
+    try:
+        password_length = int(user_answer)
+        if type(password_length) == int:
+            print(createPassword(password_length))
+    except ValueError:
+        print("Invalid input. Please enter a valid integer")
+        main()
+
+main()
